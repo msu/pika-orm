f0a465fd977e2f370bead42c11be30951d943f85
Carson Gross <carson@bigsky.software>
2025-06-04 10:35:54 -0600

    test dates

--- src/test/java/grug/db/DataTypesTest.java
+++ src/test/java/grug/db/DataTypesTest.java
@@ -1,10 +1,12 @@
 package grug.db;
 
+import grug.db.models.HasDate;
 import grug.db.models.HasEnum;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.Date;
 
 import static grug.db.models.HasEnum.MyEnum.BAR;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -24,17 +26,34 @@ public class DataTypesTest extends TestBase {
                     my_enum TEXT NOT NULL
                 );
                 """);
+
+        orm.exec("""
+                CREATE TABLE IF NOT EXISTS has_date (
+                    id INTEGER PRIMARY KEY,
+                    date INTEGER NOT NULL
+                );
+                """);
     }
 
-
-        @Test
+    @Test
     void enumsSerializeAndDeserialize() {
-            HasEnum hasEnum = new HasEnum();
-            hasEnum.setMyEnum(BAR);
-            long id = orm.insert(hasEnum);
+        HasEnum hasEnum = new HasEnum();
+        hasEnum.setMyEnum(BAR);
+        long id = orm.insert(hasEnum);
 
-            HasEnum fromDb = orm.find(HasEnum.class, id);
-            assertEquals(BAR, fromDb.getMyEnum());
-        }
+        HasEnum fromDb = orm.find(HasEnum.class, id);
+        assertEquals(BAR, fromDb.getMyEnum());
+    }
+
+    @Test
+    void datesSerializeAndDeserialize() {
+        HasDate hasDate = new HasDate();
+        Date date = new Date();
+        hasDate.setDate(date);
+        long id = orm.insert(hasDate);
+
+        HasDate fromDb = orm.find(HasDate.class, id);
+        assertEquals(date, fromDb.getDate());
+    }
 
 }
---
+++ src/test/java/grug/db/models/HasDate.java
@@ -0,0 +1,25 @@
+package grug.db.models;
+
+import java.util.Date;
+
+public final class HasDate {
+    private int id;
+    private Date date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+}
